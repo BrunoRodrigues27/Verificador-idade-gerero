f8191f4aeb3d463fbee112408329eb2f892d36f1
Bruno Rodrigues <bruno.rodrigues201800@gmail.com>
2025-02-11 00:48:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let btn = document.querySelector('input#btn')
 let perfil = document.getElementById('foto')
 let ano_atual = new Date().getFullYear()
-
+let secao = document.getElementById('imagem')
 btn.addEventListener('click', verificar)
 let genero = ''
 
@@ -10,9 +10,6 @@ function verificar() {
     let idade = ano_atual - anoN 
     let msg = document.getElementById('texto')
     let sexo = document.getElementsByName('sexo')
-    perfil.style.height = '300px'
-    perfil.style.width = '300px'
-    perfil.style.borderRadius = '50%'
     
 
     if (anoN == '' || idade >= 99 || anoN > ano_atual || anoN < 0){
@@ -20,38 +17,81 @@ function verificar() {
         return;
     }
 
-    if (sexo[0].checked && idade < 12) {
+    if (idade >= 0 && idade <= 1 && sexo[0].checked){
+        msg.innerHTML = `Homem de ${idade} ano detectado`
+        perfil.src = 'menino.jpg'
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
+        return;
+    }
+
+    if (idade >= 0 && idade <= 1 && sexo[1].checked){
+        msg.innerHTML = `Mulher de ${idade} ano detectado`
+        perfil.src = 'menina.jpg'
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
+        return;
+    }
+    
+    if (sexo[0].checked && idade > 1 && idade < 12) {
         msg.innerHTML = `Homem de ${idade} anos detectado`
         perfil.src = 'menino.jpg'
-        perfil.style.border = 'solid 3px white'
-        
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
     } else if (sexo[0].checked && idade <= 21) {
         msg.innerHTML = `Homem de ${idade} anos detectado`
         perfil.src = 'adolescente.jpg'
-        perfil.style.border = 'solid 3px white'
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
     } else if(sexo[0].checked && idade > 21 && idade <= 45){
         msg.innerHTML = `Homem de ${idade} anos detectado`
         perfil.src = 'homem.jpg'
-        perfil.style.border = 'solid 3px white'
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
     } else if(sexo[0].checked && idade > 45){
         msg.innerHTML = `Homem de ${idade} anos detectado`
         perfil.src = 'idoso.jpg'
-        perfil.style.border = 'solid 3px white'
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
    } else if (sexo[1].checked && idade < 12) {
         msg.innerHTML = `Mulher de ${idade} anos detectada`
         perfil.src = 'menina.jpg'
-        perfil.style.border = 'solid 3px white'
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
     } else if (sexo[1].checked && idade <= 21) {
         msg.innerHTML = `Mulher de ${idade} anos detectada`
         perfil.src = 'adolescentem.jpg'
-        perfil.style.border = 'solid 3px white'
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
     } else if(sexo[1].checked && idade > 21 && idade <= 45){
         msg.innerHTML = `Mulher de ${idade} anos detectada`
         perfil.src = 'Mulher.jpg'
-        perfil.style.border = 'solid 3px white'
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
     } else if(sexo[1].checked && idade > 45){
         msg.innerHTML = `Mulher de ${idade} anos detectada`
         perfil.src = 'idosa.jpg'
-        perfil.style.border = 'solid 3px white'
+        perfil.style.border = 'solid 3px black'
+        perfil.style.height = '300px'
+        perfil.style.width = '300px'
+        perfil.style.borderRadius = '50%'
     }
 }
